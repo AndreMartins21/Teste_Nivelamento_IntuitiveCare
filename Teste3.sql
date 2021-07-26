fd6b0b194598040265b36bd4413bc3892b4b7904
André Augusto Moreira Martins <64978311+AndreMartins21@users.noreply.github.com>
2021-07-25 23:34:31 -0300

    Update Teste3

--- Teste3.sql
+++ Teste3.sql
@@ -3,58 +3,123 @@ CREATE DATABASE IF NOT EXISTS intuitivecare_andre;
 
 use intuitivecare_andre;
 
-# Criação da tabela correspondente ao 'relatorio_cadop'
+# -> QUERIE DE LOAD (Relatório Cadop):
+
 CREATE TABLE IF NOT exists relatorio (
-`Registro` int PRIMARY KEY,
-`CNPJ` varchar(50),
+`Registro` varchar(50) PRIMARY KEY,
+`CNPJ` varchar(255),
 `Razão Social` varchar(255),
 `Nome Fantasia` varchar(255),
-`Modalidade` varchar(150),
+`Modalidade` varchar(255),
 `Logradouro` varchar(255),
-`Número` varchar(150),
+`Número` varchar(50),
 `Complemento` varchar(255),
-`Bairro` varchar(150),
-`Cidade` varchar(150),
+`Bairro` varchar(255),
+`Cidade` varchar(50),
 `UF` varchar(2),
-`CEP` int,
-`DDD` int,
-`Telefone` varchar(100),
-`FAX` varchar(50),
-`Endereco Eletronico` varchar(150),
+`CEP` varchar(255),
+`DDD` varchar(5),
+`Telefone` varchar(50),
+`FAX` varchar(255),
+`Endereco Eletronico` varchar(255),
 `Representante` varchar(255),
 `Cargo Representante` varchar(255),
-`Data Registro ANS` varchar(255)
-);
+`Data Registro ANS` varchar(50)
+);	
+# P.S: A maioria das atribuições às colunas dessa tabela estava gerando erro ao colocar valores como INT. Para evitá-los atribui VARCHAR() em tais. 
 
-# Criação da tabela para o preenchimento do CSV com as informações do Trimestre
-CREATE TABLE IF NOT EXISTS demo(
+LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Intuitive Care/Relatorio_cadop.csv"
+INTO TABLE relatorio
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+select count(*) from relatorio;
+
+# -> QUERIE DE LOAD 2 (Demonstrações Contabeis):
+
+CREATE TABLE IF NOT EXISTS demo (
 `DATA` varchar(50),
 `REG_ANS` varchar(50) NOT NULL,
-`CD_CONTA_CONTABIL`varchar(150),
+`CD_CONTA_CONTABIL` varchar(150),
 `DESCRICAO` varchar(255),
-`VL_SALDO_FINAL` varchar(100)
-); # Não tem Key Primária
+`VL_SALDO_FINAL` varchar(50)
+);
+# <> OBSERVAÇÕES: 
+# Não tem Key Primária;
+# O tipo primitivo do campo `VL_SALDO_FINAL` passará a ser DOUBLE na Query Analítica. Isso porque estava ocorrendo alguns erros ao tentar setar tal tipo primitivo 
 
 
-# QUERIE DE LOAD:
--- Devido ao comando `LOAD DATA INFILE` não estar funcionando em minha máquina, importei os arquivos CSV'S através da ferramenta "Table Data Import Wizard " 
---do MySQL Workbench. Com ela, os dados já são automaticamente inseridos na tabela selecionada
-# Relatorio_cadop = OK
-# CSV 1T2021 = Uma amostra dos dados OK
-SELECT * FROM relatorio;
-SELECT * FROM demo;
+# 1T2021 - 688.887 rows
+LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Intuitive Care/1T2021.csv"
+INTO TABLE demo
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
 
 
--- Observação: DO CSV 1T2021 consegui obter 75131 dados, haja vista alguns problemas que tive para inserir o restante dos dados... 
-select count(*) from demo;
+# 4T2020 - 865.171 rows
+LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Intuitive Care/4T2020.csv"
+INTO TABLE demo
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
 
 
-# QUERIE ANALÍTICA
-# 1° Questão: 10 operadoras que mais tiveram despesas com "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR" no último trimestre
-SELECT demo.`REG_ANS`, relatorio.`Razão Social`, SUM(demo.`VL_SALDO_FINAL`) AS `VALOR_TOTAL_TRIM`, demo.descricao from demo 
+# 3T2020 - 686.756 rows
+LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Intuitive Care/3T2020.csv"
+INTO TABLE demo
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS; 
+
+
+# 2T2020 - 672.239 rows
+LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Intuitive Care/2T2020.csv"
+INTO TABLE demo
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+
+# 1T2020 - 679.640
+LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Intuitive Care/1T2020.csv"
+INTO TABLE demo
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+
+# TOTAL DE DADOS => 3.592.694 
+select count(*) as `TOTAL_DE_DADOS` from demo;
+
+
+# -> QUERIE ANALÍTICA
+
+# 1°) 10 operadoras que mais tiveram despesas com "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR" no último trimestre
+SELECT demo.`REG_ANS`, relatorio.`Razão Social` as `Operadoras`, demo.`VL_SALDO_FINAL` as `VALOR_TOTAL_TRIM`, demo.descricao from demo 
 JOIN relatorio 
 ON demo.`REG_ANS` = relatorio.Registro
-WHERE descricao LIKE 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR%'
-GROUP BY demo.`REG_ANS`
-ORDER BY `VALOR_TOTAL_TRIM` DESC
+WHERE `DESCRICAO` = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR ' and `DATA` = "01/01/2021"
+ORDER BY CAST(REPLACE(`VL_SALDO_FINAL`, ',', '.') AS DOUBLE)
+limit 10;
+
+# 2°) 10 operadoras que mais tiveram despesas com "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR" no último ano?
+SELECT demo.`REG_ANS`, relatorio.`Razão Social` as `Operadoras`, demo.`VL_SALDO_FINAL` as `VALOR_TOTAL_TRIM`, demo.descricao from demo 
+JOIN relatorio 
+ON demo.`REG_ANS` = relatorio.Registro
+WHERE `DESCRICAO` = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR ' and `DATA` LIKE "%2020"
+ORDER BY CAST(REPLACE(`VL_SALDO_FINAL`, ',', '.') AS DOUBLE)
 limit 10;
